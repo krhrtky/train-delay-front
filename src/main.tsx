42a135986960c73b60bd40d2b5e524fdd08a5dae
t-kurihara <takuya.kurihara00@gmail.com>
2019-10-27 19:53:23 +0900

    [add] search by keyword.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { render } from "react-dom";
 import Amplify from 'aws-amplify'
 import { API } from 'aws-amplify'
+import { ChangeEvent } from 'react'
 
 Amplify.configure({
   API: {
@@ -21,6 +22,8 @@ type Line = {
 
 interface LinesState {
   lines: Array<Line>
+  loading: boolean
+  keyword: string
 }
 
 interface LineProps {
@@ -28,23 +31,38 @@ interface LineProps {
   notice: boolean
   handleClick: (e: any) => Promise<void>
 }
+
 const Line = (props: LineProps) => <h2>
   {props.name}:
   <button onClick={props.handleClick}>
     {props.notice ? '非通知にする' : '通知する'}
   </button>
 </h2>
+
+interface SearchProcess {
+  handleInput: (e: any) => void
+}
+
+const Search = (props: SearchProcess) =>
+  <label>キーワード
+    <input type="text" name="keyword" onChange={props.handleInput}/>
+  </label>
+
 const Main = () => <h1>遅延バッチ</h1>
 class App extends React.Component<any, LinesState> {
   constructor(props) {
     super(props)
     this.state = {
-      lines: []
+      lines: [],
+      loading: false,
+      keyword: ''
     }
     this.update = this.update.bind(this)
+    this.inputKeyword = this.inputKeyword.bind(this)
   }
 
   async update() {
+    this.setState({ loading: true })
     try {
       const { data } = await API.get("APIGatewayAPI", "/lines", { response: true })
       this.setState({
@@ -53,6 +71,11 @@ class App extends React.Component<any, LinesState> {
     } catch (e) {
       console.error(e)
     }
+    this.setState({ loading: false })
+  }
+
+  async componentDidMount(): Promise<void> {
+    await this.update()
   }
 
   async changeNotice(props) {
@@ -75,19 +98,36 @@ class App extends React.Component<any, LinesState> {
     })
   }
 
+  inputKeyword(e: ChangeEvent) {
+    const keyword = (e.target as HTMLInputElement).value
+    this.setState({keyword})
+  }
+
   render() {
     return <>
       <Main/>
+      <Search handleInput={this.inputKeyword}/>
       <button onClick={this.update}>update</button>
+
       {
-        this.state.lines.map((line, i) => <Line
-          name={line.name}
-          notice={line.notice}
-          key={i}
-          handleClick={this.changeNotice.bind(this, line)}
-        />
-      )}
-      </>
+        this.state.loading
+          ? <p>loading</p>
+          : this.state.lines.filter(
+            line => line.name.match(
+              new RegExp(
+                this.state.keyword
+                  .replace(/[\\^$.*+?()[\]{}|]/g, '.*\\$&.*')
+              )
+            )
+          )
+            .map((line, i) => <Line
+                name={line.name}
+                notice={line.notice}
+                key={i}
+                handleClick={this.changeNotice.bind(this, line)}
+              />
+            )}
+    </>
   }
 }
 
