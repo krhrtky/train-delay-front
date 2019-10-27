c29ad9f1ef6c06ae6ae8a857d310df2ddfd783f6
t-kurihara <takuya.kurihara00@gmail.com>
2019-10-27 19:53:37 +0900

    [add] dev json server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,40 @@
 /** @type import('webpack').Configuration */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/main.tsx',
-  output: {
-    path: `${__dirname}/dist`,
-    filename: 'main.[hash].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-      },
+module.exports = (env, argv) => {
+  const mode = argv.mode;
+  const isProduction = mode === 'production';
+
+  return {
+    mode: mode,
+    entry: './src/main.tsx',
+    output: {
+      path: `${__dirname}/dist`,
+      filename: 'main.[hash].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: isProduction ? './index.html' : './dev.html',
+      }),
+      new webpack.DefinePlugin({
+        'process.env.API_END_POINT': JSON.stringify(process.env.API_END_POINT),
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-  ],
-  devServer: {
-    open: true,
-  },
+    devServer: {
+      open: true,
+    },
+    devtool: "source-map"
+  };
 };
