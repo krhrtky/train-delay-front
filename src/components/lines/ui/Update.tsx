59efa3d3c6d8dc5b146c92aae3514f7c1b6a8cb7
t-kurihara <takuya.kurihara00@gmail.com>
2019-11-04 17:53:57 +0900

    [change] button ui.

--- src/components/lines/ui/Update.tsx
+++ src/components/lines/ui/Update.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react'
+import { Button } from '@material-ui/core'
 
 interface UpdateProcess {
   handleClick: () => Promise<void>
 }
 
-const Update = (props: UpdateProcess) => <button onClick={props.handleClick}>最新化</button>
+const Update: React.FC<UpdateProcess> = props =>
+  <Button
+    variant="contained"
+    color="primary"
+    size="small"
+    onClick={props.handleClick}
+  >
+    最新化
+  </Button>
 
 export default Update
